bba4360ce2b21bbf7de5aba686462312a0fa91bb
Zachary Carter <carterza@gmail.com>
2017-03-19 21:36:42 -0400

    Adding android example 00 and updating nakefile for android examples.

--- nakefile.nim
+++ nakefile.nim
@@ -2,25 +2,43 @@ import algorithm, nake, os, strutils
 
 const
   fragBin = "src/frag"
-  exDir = "examples/desktop"
+  desktopExDir = "examples/desktop"
+  androidExDir = "examples/android"
   exBin = "main"
 
 proc compile(bin: string) = direShell(nimExe, "c", bin)
 proc run(bin: string) = direShell(nimExe, "c", "-r", bin)
-proc runExample(name: string) = run(join(@[ exDir, name, exBin ], "/"))
+proc runDesktopExample(name: string) = run(join(@[ desktopExDir, name, exBin ], "/"))
+proc runAndroidExample(name: string) = run(join(@[ androidExDir, name, exBin ], "/"))
 
 proc registerExample(name, path: string) =
   var parts = path.split('-')
   let id = parts[0]
   parts.delete(0)
-  task id, name & " - run example " & parts.join("-"):
-    runExample(path)
+  task name[0] & id, name & " : run example " & parts.join("-"):
+    case name[0]
+    of 'D':
+      runDesktopExample(path)
+    of 'A':
+      runAndroidExample(path)
+    else:
+      discard
 
-var examples: seq[string] = @[]
+var desktopExamples: seq[string] = @[]
+var androidExamples: seq[string] = @[]
+
 for kind, path in walkDir("examples/desktop", true):
   if path.contains("assets"): continue
-  examples.add(path)
-sort(examples, cmp[string])
+  desktopExamples.add(path)
+sort(desktopExamples, cmp[string])
 
-for path in examples:
+for kind, path in walkDir("examples/android", true):
+  if path.contains("assets"): continue
+  androidExamples.add(path)
+sort(androidExamples, cmp[string])
+
+for path in desktopExamples:
   registerExample("Desktop", path)
+
+for path in androidExamples:
+  registerExample("Android", path)
