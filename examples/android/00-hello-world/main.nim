d2d8a7cab7060d1f745bf4589b5a7f1d0b517482
Zachary Carter <carterza@gmail.com>
2017-03-20 02:07:25 -0400

    Android gitignore additions, updating android hello world example, fixing android logging.

--- examples/android/00-hello-world/main.nim
+++ examples/android/00-hello-world/main.nim
@@ -1,12 +1,36 @@
 import
+  ../../../src/frag,
   ../../../src/frag/config,
-  ../../../src/frag,
-  ../../../src/frag,
-  #../../../src/frag/graphics/color,
-  ../../../src/frag/graphics/window
+  ../../../src/frag/graphics/window,
+  ../../../src/frag/logger,
+  ../../../src/frag/modules/graphics
 
 type
   App = ref object
+
+proc initializeApp(app: App, ctx: Frag) =
+  logDebug "Initializing app..."
+  logDebug "App initialized."
+
+proc updateApp(app:App, ctx: Frag, deltaTime: float) =
+  discard
+
+proc renderApp(app: App, ctx: Frag) =
+  ctx.graphics.clearView(0, ClearMode.Color.ord or ClearMode.Depth.ord, 0x303030ff, 1.0, 0)
+
+proc shutdownApp(app: App, ctx: Frag) =
+  logDebug "Shutting down app..."
+  logDebug "App shut down."
+
+startFrag[App](Config(
+  rootWindowTitle: "Frag Example 00-hello-world",
+  rootWindowPosX: window.posUndefined, rootWindowPosY: window.posUndefined,
+  rootWindowWidth: 960, rootWindowHeight: 540,
+  resetFlags: ResetFlag.VSync,
+  logFileName: "example-00.log",
+  assetRoot: "../assets",
+  debugMode: DebugMode.Text
+))
 
 {.emit: """
 #include <SDL_main.h>
@@ -26,29 +50,3 @@ int main(int argc, char** args) {
 }
 
 """.}
-
-logDroid "HERE"
-#logDroid repr bgfx_init(BGFX_RENDERER_TYPE_NOOP, 0'u16, 0, nil, nil)
-proc initializeApp*(app: App, ctx: Frag) =
-  logDroid "Initializing app..."
-  logDroid "App initialized."
-
-proc updateApp*(app:App, ctx: Frag, deltaTime: float) =
-  discard
-
-proc renderApp*(app: App, ctx: Frag) =
-  ctx.graphics.clearView(0, graphics.ClearMode.Color.ord or graphics.ClearMode.Depth.ord, 0x303030ff, 1.0, 0)
-
-proc shutdownApp*(app: App, ctx: Frag) =
-  logDroid "Shutting down app..."
-  logDroid "App shut down."
-
-startFrag[App](Config(
-  rootWindowTitle: "Frag Example 00-hello-world",
-  rootWindowPosX: window.posUndefined, rootWindowPosY: window.posUndefined,
-  rootWindowWidth: 960, rootWindowHeight: 540,
-  resetFlags: graphics.ResetFlag.None,
-  logFileName: "example-00.log",
-  assetRoot: "../assets",
-  debugMode: graphics.DebugMode.Text
-))
--- src/frag/logger.nim
+++ src/frag/logger.nim
@@ -1,47 +1,52 @@
 import
-  logging
+  logging,
+  strutils
 
 when defined(android):
   {.emit: """
     #include <android/log.h>
   """.}
-  proc native_log(level: string, a: cstring) =
-      {.emit: """__android_log_write(ANDROID_LOG_INFO, level, `a`);""".}
+  proc native_log(level: Level, a: cstring) =
+      {.emit: """__android_log_write(ANDROID_LOG_INFO, $level, `a`);""".}
 
-  proc log*(a: varargs[string, `$`]) = native_log(LevelNames.DEBUG, a.join())
-  proc logDebug*(a: varargs[string, `$`]) = native_log(LevelNames.DEBUG, a.join())
-  proc logInfo*(a: varargs[string, `$`]) = native_log(LevelNames.INFO, a.join())
-  proc logNotice*(a: varargs[string, `$`]) = native_log(LevelNames.NOTICE, a.join())
-  proc logWarn*(a: varargs[string, `$`]) = native_log(LevelNames.WARN, a.join())
-  proc logError*(a: varargs[string, `$`]) = native_log(LevelNames.ERROR, a.join())
-  proc logFatal*(a: varargs[string, `$`]) = native_log(LevelNames.FATAL, a.join())
+  proc log*(a: varargs[string, `$`]) = native_log(Level.lvlDebug, a.join())
+  proc logDebug*(a: varargs[string, `$`]) = native_log(Level.lvlDebug, a.join())
+  proc logInfo*(a: varargs[string, `$`]) = native_log(Level.lvlInfo, a.join())
+  proc logNotice*(a: varargs[string, `$`]) = native_log(Level.lvlNotice, a.join())
+  proc logWarn*(a: varargs[string, `$`]) = native_log(Level.lvlWarn, a.join())
+  proc logError*(a: varargs[string, `$`]) = native_log(Level.lvlError, a.join())
+  proc logFatal*(a: varargs[string, `$`]) = native_log(Level.lvlFatal, a.join())
 
-var consoleLogger : ConsoleLogger
-var fileLogger : FileLogger
+  proc init*(logFileName: string) =
+    discard
 
-proc log*(args: varargs[string, `$`]) =
-  logging.debug(args)
+else:
+  var consoleLogger : ConsoleLogger
+  var fileLogger : FileLogger
 
-proc logDebug*(args: varargs[string, `$`]) =
-  logging.debug(args)
+  proc log*(args: varargs[string, `$`]) =
+    logging.debug(args)
 
-proc logInfo*(args: varargs[string, `$`]) =
-  logging.info(args)
+  proc logDebug*(args: varargs[string, `$`]) =
+    logging.debug(args)
 
-proc logNotice*(args: varargs[string, `$`]) =
-  logging.notice(args)
+  proc logInfo*(args: varargs[string, `$`]) =
+    logging.info(args)
 
-proc logWarn*(args: varargs[string, `$`]) =
-  logging.warn(args)
+  proc logNotice*(args: varargs[string, `$`]) =
+    logging.notice(args)
 
-proc logError*(args: varargs[string, `$`]) =
-  logging.error(args)
+  proc logWarn*(args: varargs[string, `$`]) =
+    logging.warn(args)
 
-proc logFatal*(args: varargs[string, `$`]) =
-  logging.fatal(args)
+  proc logError*(args: varargs[string, `$`]) =
+    logging.error(args)
 
-proc init*(logFileName: string) =
-  consoleLogger = newConsoleLogger()
-  fileLogger = newFileLogger(logFileName)
-  logging.addHandler(consoleLogger)
-  logging.addHandler(fileLogger)
+  proc logFatal*(args: varargs[string, `$`]) =
+    logging.fatal(args)
+
+  proc init*(logFileName: string) =
+    consoleLogger = newConsoleLogger()
+    fileLogger = newFileLogger(logFileName)
+    logging.addHandler(consoleLogger)
+    logging.addHandler(fileLogger)
