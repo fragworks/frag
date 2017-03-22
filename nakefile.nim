610bcebd0a7d9f3fb7ee9750f68c96bf4e02f7c1
Zachary Carter <carterza@gmail.com>
2017-03-22 07:41:18 -0400

    Adding additional build targets.

--- nakefile.nim
+++ nakefile.nim
@@ -1,10 +1,11 @@
 import
   nake,
-  os
+  os,
+  strutils
 
 type
   Targets = enum
-    OSXDebug32, OSXDebug64, AndroidARM32
+    AndroidARMDebug32, OSXDebug32, OSXDebug64, LinuxDebug32, LinuxDebug64
 
 proc verifyBx(): bool =
   dirExists("vendor/bx")
@@ -16,28 +17,29 @@ proc verifyDependencies(): bool =
   if verifyBx() and verifyBgfx():
     return true
 
-proc genAndroidARMGmake() =
-  if not shell("genie --with-shared-lib --gcc=android-arm gmake"):
+proc genGmakeProjectsAndCd(gcc: string) =
+  if not shell("genie --with-shared-lib --gcc=$1 gmake" % gcc):
       echo "Ensure GENie is installed before proceeding: https://github.com/bkaradzic/GENie"
-  cd(".build/projects/gmake-android-arm")
-
-proc genOSXGmake() =
-  if not shell("genie --with-shared-lib --gcc=osx gmake"):
-      echo "Ensure GENie is installed before proceeding: https://github.com/bkaradzic/GENie"
-  cd(".build/projects/gmake-osx")
+  cd(".build/projects/gmake-$1" % gcc)
 
 proc installBgfx(target: Targets) =
   cd("vendor/bgfx")
   case target
+  of AndroidARMDebug32:
+    genGmakeProjectsAndCd("android-arm")
+    direShell("make config=debug32 bgfx-shared-lib")
   of OSXDebug32:
-    genOSXGmake()
+    genGmakeProjectsAndCd("osx")
     direShell("make config=debug32 bgfx-shared-lib")
   of OSXDebug64:
-    genOSXGmake()
+    genGmakeProjectsAndCd("osx")
     direShell("make config=debug64 bgfx-shared-lib")
-  of AndroidARM32:
-    genAndroidARMGmake()
+  of LinuxDebug32:
+    genGmakeProjectsAndCd("linux")
     direShell("make config=debug32 bgfx-shared-lib")
+  of LinuxDebug64:
+    genGmakeProjectsAndCd("linux")
+    direShell("make config=debug64 bgfx-shared-lib")
 
 proc installDependencies(target: Targets) =
   installBgfx(target)
@@ -54,9 +56,18 @@ if not verifyDependencies():
 ###########
 # ANDROID #
 ###########
-task "android-arm32", "Build debug versions of FRAG dependencies for ARM 32-bit instruction set":
+task "android-arm-debug32", "Build debug versions of FRAG dependencies for ARM 32-bit instruction set":
   verifyAndroidEnvVars()
-  installDependencies(AndroidARM32)
+  installDependencies(AndroidARMDebug32)
+
+#########
+# LINUX #
+#########
+task "linux-debug32", "Build debug verisons of FRAG dependencies for Linux 32-bit instruction set":
+  installDependencies(LinuxDebug32)
+  
+task "linux-debug64", "Build debug verisons of FRAG dependencies for Linux 64-bit instruction set":
+  installDependencies(LinuxDebug64)
 
 #######
 # OSX #
